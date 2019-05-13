4eccae77935d703d2bb0004e07aa4f2c8431c4fe
Ian Gacek <gacek004@umn.edu>
2019-05-13 16:43:44 -0500

    added code similar to class activities

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+    $("button").on("click", function() {
+        var animal = $(this).attr("data-animal");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          animal + "&api_key=qXmzGsZI4v039l2oVSrlHS3AJZAY0mDC&limit=10";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            console.log(queryURL);
+  
+            console.log(response);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+              var animalDiv = $("<div>");
+              var p = $("<p>").text("Rating: " + results[i].rating);
+              var animalImage = $("<img>");
+              animalImage.attr("src", results[i].images.fixed_height.url);
+              animalDiv.append(p);
+              animalDiv.append(animalImage);
+              $("#gifs-appear-here").prepend(animalDiv);
+            }
+          });
+      });
