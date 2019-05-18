e9e05d3fc350bf1d6d2815077b7b372a467dbbf7
Ian <gacek004@umn.edu>
2019-05-18 17:47:34 -0500

    fixed buttons appearing in javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,42 @@
-    $("button").on("click", function() {
-        var animal = $(this).attr("data-animal");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-          animal + "&api_key=qXmzGsZI4v039l2oVSrlHS3AJZAY0mDC&limit=10";
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-          .then(function(response) {
-            console.log(queryURL);
-  
-            console.log(response);
-            var results = response.data;
-            for (var i = 0; i < results.length; i++) {
-              var animalDiv = $("<div>");
-              var p = $("<p>").text("Rating: " + results[i].rating);
-              var animalImage = $("<img>");
-              animalImage.attr("src", results[i].images.fixed_height.url);
-              animalDiv.append(p);
-              animalDiv.append(animalImage);
-              $("#gifs-appear-here").prepend(animalDiv);
-            }
-          });
-      });
+var animals = ["Dog", "Cat", "Bird", "Snake", "Giraffe", "Mouse", "Fish", "Bug",];
+
+function renderButtons() {
+  $("#buttons-view").empty();
+  for (var i = 0; i < animals.length; i++) {
+    var a = $("<button class='btn btn-primary'>");
+    a.addClass("animals");
+    a.attr("data-animal", animals[i]);
+    a.text(animals[i]);
+    $("#buttons-view").append(a);
+  }
+}
+
+renderButtons();
+
+$("button").on("click", function () {
+  var animal = $(this).attr("data-animal");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    animal + "&api_key=qXmzGsZI4v039l2oVSrlHS3AJZAY0mDC&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(queryURL);
+
+      console.log(response);
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var animalDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var animalImage = $("<img>");
+        animalImage.attr("src", results[i].images.fixed_height.url);
+        animalDiv.append(p);
+        animalDiv.append(animalImage);
+        $("#gifs-appear-here").prepend(animalDiv);
+      }
+    });
+});
 
 // **CODE FROM IN-CLASS EXAMPLE, USE FOR REFERENCE/GUIDANCE**
 // 	var searchTerms = ['fox', 'cow', 'chicken', 'pig', 'lizard'];
