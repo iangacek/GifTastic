49c8c1d9b69ac30fa857e630c3de813a218b4117
Ian <gacek004@umn.edu>
2019-05-18 19:45:12 -0500

    added null code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,46 +1,60 @@
-var animals = ["Dog", "Cat", "Bird", "Snake", "Giraffe", "Mouse", "Fish", "Bug",];
+$(document).ready(function () {
 
-function renderButtons() {
-  $("#buttons-view").empty();
-  for (var i = 0; i < animals.length; i++) {
-    var a = $("<button class='btn btn-primary'>");
-    a.addClass("animal-btn");
-    a.attr("data-animal", animals[i]);
-    a.text(animals[i]);
-    $("#buttons-view").append(a);
+  var animals = ["Dog", "Cat", "Bird", "Snake", "Giraffe", "Mouse", "Fish", "Bug",];
+
+
+  // Renders buttons for animals already in array
+  function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < animals.length; i++) {
+      var a = $("<button class='btn btn-primary'>");
+      a.addClass("animal-btn");
+      a.attr("data-animal", animals[i]);
+      a.text(animals[i]);
+      $("#buttons-view").append(a);
+    }
   }
-}
 
-renderButtons();
-
-$("#add-animal").on("click", function(event){
-  event.preventDefault();
-  var animal = $("#animal-input").val().trim();
-  animals.push(animal);
   renderButtons();
-})
 
-
-function displayAnimalInfo () {
-  var animal = $(this).attr("data-animal");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    animal + "&api_key=qXmzGsZI4v039l2oVSrlHS3AJZAY0mDC&limit=10";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
+  // Adds additional buttons in search box
+  $("#add-animal").on("click", function (event) {
+    event.preventDefault();
+    var animal = $("#animal-input").val().trim();
+    animals.push(animal);
+    renderButtons();
   })
-    .then(function (response) {
-      var results = response.data;
-      for (var i = 0; i < results.length; i++) {
-        var animalDiv = $("<div>");
-        var p = $("<p>").text("Rating: " + results[i].rating);
-        var animalImage = $("<img>");
-        animalImage.attr("src", results[i].images.fixed_height.url);
-        animalDiv.append(p);
-        animalDiv.append(animalImage);
-        $("#gifs-appear-here").prepend(animalDiv);
-      }
-    });
-};
 
-$(document).on("click", ".animal-btn", displayAnimalInfo);
+// Displays GIFs
+  function displayAnimalInfo() {
+    var animal = $(this).attr("data-animal");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=qXmzGsZI4v039l2oVSrlHS3AJZAY0mDC&limit=10";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function (response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+          var animalDiv = $("<div>");
+          var p = $("<p>").text("Rating: " + results[i].rating);
+          var animalImage = $("<img>");
+          animalImage.attr("src", results[i].images.fixed_height.url);
+          animalDiv.append(p);
+          animalDiv.append(animalImage);
+          $("#gifs-appear-here").prepend(animalDiv);
+        }
+      });
+    // if (state === "still") {
+    //   $(this).attr("src", $(this).attr("data-animate"));
+    //   $(this).attr("data-state", "animate");
+    // } else {
+    //   $(this).attr("src", $(this).attr("data-still"));
+    //   $(this).attr("data-state", "still");
+    // }
+  };
+
+  // Actions on buttons to display GIFs
+  $(document).on("click", ".animal-btn", displayAnimalInfo);
+});
